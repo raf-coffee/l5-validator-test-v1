767c399090f86c315cf2ab8128d4441ff3b93c9b
raf-coffee <pavel.hexlet@mail.ru>
2023-10-23 14:27:10 +0300

    task 4

---
+++ src/ArraySchema.js
@@ -0,0 +1,13 @@
+export default class ArraySchema{
+  constructor(validators = [(arr) => Array.isArray(arr)]) {
+    this.validators = validators;
+  }
+
+  isValid(arr) {
+    return this.validators.every((validator) => validator(arr));
+  }
+
+  length(len) {
+    return new ArraySchema([...this.validators, (arr) => arr.length === len])
+  }
+}
--- src/Validator.js
+++ src/Validator.js
@@ -1,26 +1,41 @@
+import ArraySchema from './ArraySchema.js';
 import NumberSchema from './NumberSchema.js';
 import StringSchema from './StringSchema.js';
 
 export default class Validator {
-  // eslint-disable-next-line class-methods-use-this
+  /* eslint-disable */
   string() {
     return new StringSchema();
   }
 
-  // eslint-disable-next-line class-methods-use-this
   number() {
     return new NumberSchema();
   }
+
+  array() {
+    return new ArraySchema();
+  }
+
+  /* eslint-enable */
 }
 
+// TASK 1
+// const v = new Validator();
+// console.log(v.string().isValid(3));
+
+// TASK 2
 // const validator = new Validator();
 // const schema1 = validator.number().even();
 // const schema2 = validator.number().odd();
-
 // console.log(schema1.isValid(''))
 // console.log(schema1.isValid(2))
 // console.log(schema2.isValid(2))
 // console.log(schema2.isValid(3))
 
-// const v = new Validator();
-// console.log(v.string().isValid(3));
+// TASK 3
+// const validator = new Validator();
+// const schema = validator.array();
+// console.log(schema.isValid('Hello!'))
+// console.log(schema.isValid(123))
+// console.log(schema.isValid([]))
+// console.log(schema.isValid([1, 23, 4]))
